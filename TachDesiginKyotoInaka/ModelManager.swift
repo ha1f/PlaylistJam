25c691a9689eb3d2823c6427c2fda8165e214d96
ha1fha1f <half.ldk.hiz@gmail.com>
2015-09-10 19:48:39 +0900

    change file

---
+++ TachDesiginKyotoInaka/ModelManager.swift
@@ -0,0 +1,17 @@
+//
+//  ModelManager.swift
+//  CaIntern
+//
+//  Created by 山口 智生 on 2015/09/10.
+//  Copyright © 2015年 NextVanguard. All rights reserved.
+//
+
+import Foundation
+
+/** インスタンスの管理 */
+class ModelManager: NSObject {
+    static let modelManager: ModelManager! = ModelManager()
+    
+    
+    
+}
