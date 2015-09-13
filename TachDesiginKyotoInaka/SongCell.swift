dbda5539683ef1b36cfe4f77143a70c3e850fe73
ganmacs <ganmacs@gmail.com>
2015-09-13 17:23:58 +0900

    add songs columns

--- TachDesiginKyotoInaka/SongCell.swift
+++ TachDesiginKyotoInaka/SongCell.swift
@@ -1,20 +1,21 @@
-//
-//  SongCell.swift
-//  TachDesiginKyotoInaka
-//
-//  Created by 山口 智生 on 2015/09/11.
-//  Copyright (c) 2015年 NextVanguard. All rights reserved.
-//
 
 import UIKit
 
 class SongCell:UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
-    var jacekt: UIImageView!
-    
-    func setSong(song: Song) {
+    @IBOutlet weak var artwork: UIImageView!
+    @IBOutlet weak var artistLabel: UILabel!
+
+    func setup(song: Song) {
+        self.backgroundColor = UIColor.blackColor()
+        setupSong(song)
+    }
+
+    private func setupSong(song: Song) {
+        self.artwork.sd_setImageWithURL(NSURL(string: song.artworkUrl)!)
+        self.artistLabel.text = song.artist
+        self.artistLabel.textColor = UIColor.whiteColor()
         self.titleLabel.text = song.title
-        self.backgroundColor = UIColor.darkGrayColor()
         self.titleLabel.textColor = UIColor.whiteColor()
     }
 }
