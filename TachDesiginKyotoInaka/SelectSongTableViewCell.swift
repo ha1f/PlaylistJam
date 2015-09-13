74e2c021aad3e761caaca94ad76ca6de84383643
坂本時緒 <tokio@tokki-mac.local>
2015-09-13 16:29:10 +0900

    ８曲選択の画面のおおまかな見ため実装

---
+++ TachDesiginKyotoInaka/SelectSongTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  SelectSongTableViewCell.swift
+//  TachDesiginKyotoInaka
+//
+//  Created by 坂本時緒 on 9/13/15.
+//  Copyright (c) 2015 NextVanguard. All rights reserved.
+//
+
+import UIKit
+
+class SelectSongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var artWorkImage: UIImageView!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var songNameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setSong(song: Song){
+        songNameLabel.text = song.title
+        artistNameLabel.text = song.artist
+        artWorkImage.image = UIImage(named: "art.jpg")
+        
+    }
+}
